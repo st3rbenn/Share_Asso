0398be9b2e1967f5e007b5b5d70438209b23daa1
Anthonin <Anthonincolas37000@gmail.com>
2022-08-31 12:33:50 +0200

    adding form style with tailwind

--- src/Form/RegisterType.php
+++ src/Form/RegisterType.php
@@ -23,10 +23,12 @@ class RegisterType extends AbstractType
             'required' => true,
             'attr' => [
                 'placeholder' => 'Renseignez votre email',
+                'class' => 'mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm',
             ],
         ])
         
         ->add('password', RepeatedType::class, [
+            'label' => '',
             'type' => PasswordType::class,
             'invalid_message' => 'Les mots de passe doivent correspondre',
             'required' => true,
@@ -34,12 +36,14 @@ class RegisterType extends AbstractType
                 'label' => 'Mot de passe',
                 'attr' => [
                     'placeholder' => 'Renseignez votre mot de passe',
+                    'class' => 'mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm',
                 ],
             ],
             'second_options' => [
                 'label' => 'Confirmation du mot de passe',
                 'attr' => [
                     'placeholder' => 'Confirmation du mot de passe',
+                    'class' => 'mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm',
                 ],
             ],
             'constraints' => [
@@ -55,6 +59,7 @@ class RegisterType extends AbstractType
                 'required' => true,
                 'attr' => [
                     'placeholder' => 'Prénom',
+                    'class' => 'mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm',
                 ],
             ])
 
@@ -63,18 +68,18 @@ class RegisterType extends AbstractType
                 'required' => true,
                 'attr' =>[
                     'placeholder' => 'Nom de famille',
+                    'class' => 'mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm',
                 ],
             ])
 
             ->add('submit', SubmitType::class, [
                 'label' => 'S\'inscrire',
                 'attr' => [
-                    'class' => 'btn btn-primary',
+                    'class' => 'ml-3 inline-flex justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2',
                 ],
             ])
         ;
     }
-
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
