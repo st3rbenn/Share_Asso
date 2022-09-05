0c9f2b39c9ddccca39308c6a3351d841e8dbe118
st3rbenn <anthonincolas37000@gmail.com>
2022-09-05 16:16:05 +0200

    adding style for the user form and file upload for user avatar

--- src/Form/RegisterType.php
+++ src/Form/RegisterType.php
@@ -5,12 +5,14 @@ namespace App\Form;
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\File;
 use Symfony\Component\Validator\Constraints\Regex;
 
 class RegisterType extends AbstractType
@@ -18,42 +20,40 @@ class RegisterType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-        ->add('email', EmailType::class, [
-            'label' => 'Email',
-            'required' => true,
-            'attr' => [
-                'placeholder' => 'Renseignez votre email',
-                'class' => 'mt-2 appearance-none text-slate-900 bg-white rounded-md block w-full px-3 h-10 shadow-sm sm:text-sm focus:outline-none placeholder:text-slate-400 focus:ring-2 focus:ring-sky-500 ring-1 ring-slate-200',
-            ],
-        ])
-        
-        ->add('password', RepeatedType::class, [
-            'label' => '',
-            'type' => PasswordType::class,
-            'invalid_message' => 'Les mots de passe doivent correspondre',
-            'required' => true,
-            'first_options' => [
-                'label' => 'Mot de passe',
+            ->add('email', EmailType::class, [
+                'label' => 'Email',
+                'required' => true,
                 'attr' => [
-                    'placeholder' => 'Renseignez votre mot de passe',
+                    'placeholder' => 'Renseignez votre email',
                     'class' => 'mt-2 appearance-none text-slate-900 bg-white rounded-md block w-full px-3 h-10 shadow-sm sm:text-sm focus:outline-none placeholder:text-slate-400 focus:ring-2 focus:ring-sky-500 ring-1 ring-slate-200',
                 ],
-            ],
-            'second_options' => [
-                'label' => 'Confirmation du mot de passe',
-                'attr' => [
-                    'placeholder' => 'Confirmation du mot de passe',
-                    'class' => 'mt-2 appearance-none text-slate-900 bg-white rounded-md block w-full px-3 h-10 shadow-sm sm:text-sm focus:outline-none placeholder:text-slate-400 focus:ring-2 focus:ring-sky-500 ring-1 ring-slate-200',
+            ])
+            ->add('password', RepeatedType::class, [
+                'label' => '',
+                'type' => PasswordType::class,
+                'invalid_message' => 'Les mots de passe doivent correspondre',
+                'required' => true,
+                'first_options' => [
+                    'label' => 'Mot de passe',
+                    'attr' => [
+                        'placeholder' => 'Renseignez votre mot de passe',
+                        'class' => 'mt-2 appearance-none text-slate-900 bg-white rounded-md block w-full px-3 h-10 shadow-sm sm:text-sm focus:outline-none placeholder:text-slate-400 focus:ring-2 focus:ring-sky-500 ring-1 ring-slate-200',
+                    ],
                 ],
-            ],
-            'constraints' => [
-                new Regex([
-                    'pattern' => '/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/',
-                    'message' => 'Le mot de passe doit contenir au moins une majuscule, une minucsule, un chiffre, aucun caractère spécial et 8 caractères'
-                ])
-            ],
-        ])
-
+                'second_options' => [
+                    'label' => 'Confirmation du mot de passe',
+                    'attr' => [
+                        'placeholder' => 'Confirmation du mot de passe',
+                        'class' => 'mt-2 appearance-none text-slate-900 bg-white rounded-md block w-full px-3 h-10 shadow-sm sm:text-sm focus:outline-none placeholder:text-slate-400 focus:ring-2 focus:ring-sky-500 ring-1 ring-slate-200',
+                    ],
+                ],
+                'constraints' => [
+                    new Regex([
+                        'pattern' => '/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/',
+                        'message' => 'Le mot de passe doit contenir au moins une majuscule, une minucsule, un chiffre, aucun caractère spécial et 8 caractères'
+                    ])
+                ],
+            ])
             ->add('user_firstname', TextType::class, [
                 'label' => 'Prénom',
                 'required' => true,
@@ -62,25 +62,43 @@ class RegisterType extends AbstractType
                     'class' => 'mt-2 appearance-none text-slate-900 bg-white rounded-md block w-full px-3 h-10 shadow-sm sm:text-sm focus:outline-none placeholder:text-slate-400 focus:ring-2 focus:ring-sky-500 ring-1 ring-slate-200',
                 ],
             ])
-
             ->add('user_lastname', TextType::class, [
                 'label' => 'Nom de famille',
                 'required' => true,
-                'attr' =>[
+                'attr' => [
                     'placeholder' => 'Nom de famille',
                     'class' => 'mt-2 appearance-none text-slate-900 bg-white rounded-md block w-full px-3 h-10 shadow-sm sm:text-sm focus:outline-none placeholder:text-slate-400 focus:ring-2 focus:ring-sky-500 ring-1 ring-slate-200',
                 ],
             ])
-
+            ->add('user_avatar', FileType::class, [
+                'required' => false,
+                'data_class' => null,
+                'data' => null,
+                'attr' => [
+                    'class' => 'cursor-pointer relative block opacity-0 w-full h-full p-10 z-50',
+                    'type' => 'file',
+                ],
+                'constraints' => [
+                    new File([
+                        'maxSize' => '1024k',
+                        'mimeTypes' => [
+                            'image/jpeg',
+                            'image/png',
+                            'image/webp'
+                        ],
+                        'mimeTypesMessage' => 'Merci de choisir un fichier de type jpeg, png ou webp',
+                    ])
+                ],
+            ])
             ->add('submit', SubmitType::class, [
                 'label' => 'finaliser l\'inscription',
                 'attr' => [
                     'class' => 'inline-flex justify-center rounded-lg text-sm font-semibold py-2.5 px-4 bg-slate-900 text-white hover:bg-slate-700 w-full',
                     'style' => 'background-color: #fb4e17;',
                 ],
-            ])
-        ;
+            ]);
     }
+
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
